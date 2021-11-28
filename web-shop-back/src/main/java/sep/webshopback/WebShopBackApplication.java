b57a4b1728f989f3fd5c2725fc65bf21276f5692
milijanadj <milijana.djordjevic1998@gmail.com>
2021-11-28 16:46:15 +0100

    web shop back app

---
+++ web-shop-back/src/main/java/sep/webshopback/WebShopBackApplication.java
@@ -0,0 +1,13 @@
+package sep.webshopback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebShopBackApplication.class, args);
+    }
+
+}
---
+++ web-shop-back/src/test/java/sep/webshopback/WebShopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package sep.webshopback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
