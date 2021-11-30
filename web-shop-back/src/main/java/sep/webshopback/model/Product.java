2d523573bb3100084925b3f7780f43e84f0fc84c
milijanadj <milijana.djordjevic1998@gmail.com>
2021-11-30 15:32:01 +0100

    store and product model

---
+++ web-shop-back/src/main/java/sep/webshopback/model/Product.java
@@ -0,0 +1,85 @@
+package sep.webshopback.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_generator")
+    @SequenceGenerator(name="product_generator", sequenceName = "product_seq", allocationSize=50, initialValue = 10)
+    private long id;
+
+    @Column(length = 50, name = "name")
+    @NotBlank
+    private String name;
+
+    @Column(name = "price")
+    @NotNull
+    private float price;
+
+    @Column(name = "quantity")
+    @NotNull
+    private long quantity;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    public Product(){}
+
+    public Product(String name, float price, long quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.imageUrl = "";
+    }
+
+    public Product(String name, float price, long quantity, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.imageUrl = imageUrl;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ web-shop-back/src/main/java/sep/webshopback/model/Store.java
@@ -0,0 +1,61 @@
+package sep.webshopback.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "stores")
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "store_generator")
+    @SequenceGenerator(name="store_generator", sequenceName = "store_seq", allocationSize=50, initialValue = 10)
+    private long id;
+
+    @Column(length = 50, name = "name")
+    @NotBlank
+    private String name;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Product> products = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private User owner;
+
+    public Store(){}
+
+    public Store(String name, List<Product> products, User owner) {
+        this.name = name;
+        this.products = products;
+        this.owner = owner;
+    }
+
+    public long getId() { return this.id; }
+
+    public void setId(long id) { this.id = id; }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
