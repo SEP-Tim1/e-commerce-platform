625d862d1ff7380d813f567b4a93ed378ee8b113
Tamara Ranković <rankovictamaraa@gmail.com>
2021-12-01 11:14:19 +0100

    shopping cart model

---
+++ web-shop-back/src/main/java/sep/webshopback/model/ProductQuantity.java
@@ -0,0 +1,50 @@
+package sep.webshopback.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product-quantity")
+public class ProductQuantity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    private Product product;
+    @Column(name="quantity")
+    private int quantity;
+
+    public ProductQuantity(long id, Product product, int quantity) {
+        this.id = id;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    protected ProductQuantity() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ web-shop-back/src/main/java/sep/webshopback/model/ShoppingCart.java
@@ -0,0 +1,62 @@
+package sep.webshopback.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "shopping-carts")
+public class ShoppingCart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    private User user;
+    @ManyToOne
+    private Store store;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<ProductQuantity> products;
+
+    public ShoppingCart(long id, User user, Store store, List<ProductQuantity> products) {
+        this.id = id;
+        this.user = user;
+        this.store = store;
+        this.products = products;
+    }
+
+    protected ShoppingCart() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public List<ProductQuantity> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductQuantity> products) {
+        this.products = products;
+    }
+}
