e93eef5af10f86d26732b118b91748156f0ab81a
turkovic98 <eminaturkovic600@gmail.com>
2021-12-01 18:18:06 +0100

    inital product images added

--- web-shop-back/src/main/resources/data.sql
+++ web-shop-back/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 insert into users(id, username, email, first_name, last_name, password, phone, role, address) values (0, 'user0', 'user0@email.com', 'Milijana', 'Djordjevic', '12345678', '+381641111111', 'USER', 'Adresa0');
 insert into users(id, username, email, first_name, last_name, password, phone, role, address) values (1, 'user1', 'user1@email.com', 'Milijana', 'Djordjevic', '12345678', '+381641111111', 'SELLER', 'Adresa1');
 insert into stores(id, name, owner_id) values (0, 'store0', 1);
-insert into products(id, name, price, quantity, image_url) values (0, 'product0', 500, 10, '');
-insert into products(id, name, price, quantity, image_url) values (1, 'product1', 1000, 1, '');
+insert into products(id, name, price, quantity, image_url) values (0, 'Jacket', 500, 10, 'storage/media-content/girl.jpg');
+insert into products(id, name, price, quantity, image_url) values (1, 'Dress', 1000, 1, 'storage/media-content/dress.jpg');
 insert into stores_products(store_id, products_id) values (0, 0);
 insert into stores_products(store_id, products_id) values (0, 1);
