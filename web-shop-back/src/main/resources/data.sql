1e1f5bab724bc1d118ec525ef2d5b1d4978ecc8f
milijanadj <milijana.djordjevic1998@gmail.com>
2021-12-12 21:44:59 +0100

    web shop seller home page

--- web-shop-back/src/main/resources/data.sql
+++ web-shop-back/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 insert into users(id, username, email, first_name, last_name, password, phone, role, address) values (0, 'user0', 'user0@email.com', 'Milijana', 'Djordjevic', '12345678', '+381641111111', 'USER', 'Adresa0');
 insert into users(id, username, email, first_name, last_name, password, phone, role, address) values (1, 'user1', 'user1@email.com', 'Milijana', 'Djordjevic', '12345678', '+381641111111', 'SELLER', 'Adresa1');
 insert into stores(id, name, owner_id) values (0, 'MOJ NOVI STOR', 1);
-insert into products(id, name, price, quantity, image_url) values (0, 'Jacket', 500, 150, 'storage/media-content/girl.jpg');
-insert into products(id, name, price, quantity, image_url) values (1, 'Dress', 1000, 10, 'storage/media-content/nocontent.jpg');
+insert into products(id, name, price, quantity, image_url) values (0, 'Mouse', 500, 150, 'storage/media-content/mis.jpg');
+insert into products(id, name, price, quantity, image_url) values (1, 'Monitor', 1000, 10, 'storage/media-content/monitor.jpg');
 insert into stores_products(store_id, products_id) values (0, 0);
 insert into stores_products(store_id, products_id) values (0, 1);
